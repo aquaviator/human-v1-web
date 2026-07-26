e3b27f58df9ddf529dd475fdabbd8ea6af985677
Andy Clarke <leatfield@gmail.com>
2026-07-26 15:02:31 +0000

    fix: add required WordPress fallback template

---
+++ wp-content/themes/human-v1-theme/index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Default template
+ *
+ * @package Human_V1
+ */
+
+get_header();
+?>
+
+<main id="primary" class="site-main">
+    <div class="container">
+
+        <?php if ( have_posts() ) : ?>
+
+            <?php while ( have_posts() ) : the_post(); ?>
+
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <h1><?php the_title(); ?></h1>
+
+                    <div class="entry-content">
+                        <?php the_content(); ?>
+                    </div>
+                </article>
+
+            <?php endwhile; ?>
+
+        <?php else : ?>
+
+            <h1>Human</h1>
+            <p>Train. Track. Transform.</p>
+
+        <?php endif; ?>
+
+    </div>
+</main>
+
+<?php
+get_footer();
